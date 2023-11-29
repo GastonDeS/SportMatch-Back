ce6b5ced74a0d600c6825a670ccfad4d31605adc
GastonDeS <65664359+GastonDeS@users.noreply.github.com>
2023-11-28 23:01:19 -0300

    chore: add logs

--- src/database/persistence/rating.persistence.ts
+++ src/database/persistence/rating.persistence.ts
@@ -6,12 +6,14 @@ import { UniqueConstraintError } from "sequelize";
 class RatingPersistence {
     static async rate(rated: string, rater: string, rating: number, eventId: string): Promise<void> {
         try {
-            await Rating.create({
+            console.log("rate params: ", rater, rated, rating, eventId);
+            const rate = await Rating.create({
                 rater: rater,
                 rated: rated,
                 rating: rating,
                 eventId: eventId
             });
+            console.log("rate object: ", rate);
         } catch (err) {
             if (err instanceof UniqueConstraintError) {
                 throw new DuplicateException("The user was already rated by the rater");
