814e59b2a88ff25ec85dca67eecda9beee0cf7ee
GastonDeS <65664359+GastonDeS@users.noreply.github.com>
2023-12-05 10:23:10 -0300

    chore: logs

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -115,8 +115,9 @@ class UsersController {
         const { phoneNumber, locations, sports } = req.body;
         const userId = req.user.id;
 
-        console.log("REQ UPDATE: ", JSON.stringify(req, null, 2));
-        console.log("REQ.BODY UPDATE: ", JSON.stringify(req.body, null, 2));
+        console.log("HELLO THERE");
+        console.log("REQ UPDATE: ", req);
+        console.log("REQ.BODY UPDATE: ", req.body);
 
         try {
             if (userIdPath !== userId) throw new Error("User can't update another user");
